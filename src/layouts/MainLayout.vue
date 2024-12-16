e0a41e94468c017434fab769e25386da424d3771
Dmitriy E <dsanichwebdev@gmail.com>
2024-12-16 21:34:42 +0300

    Initialize app

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  q-layout(view="lHh Lpr lFf")
+    q-page-container.q-pa-md(:style="'max-width: 1080px;'")
+      router-view
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+  },
+
+  data () {
+    return {}
+  },
+
+  methods: {}
+});
+</script>
---
+++ src/pages/ErrorNotFound.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  div.fullscreen.bg-blue.text-white.text-center.q-pa-md.flex.flex-center
+    div
+      div(style="font-size: 30vh") 404
+      div.text-h2(style="opacity:.4") Oops. Nothing here...
+      q-btn.q-mt-xl(
+        color="white"
+        text-color="blue"
+        unelevated
+        to="/"
+        label="Go Home"
+        no-caps
+      )
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ErrorNotFound'
+});
+</script>
