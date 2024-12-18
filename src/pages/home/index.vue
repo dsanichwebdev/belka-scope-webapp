9ec7f60fb9b6fbe7dff6b078cc2ad3efa58bd7f5
Dmitriy E <dsanichwebdev@gmail.com>
2024-12-18 21:49:36 +0300

    Added logo, title and search input to home page (landing state)

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,9 +1,21 @@
 <template lang="pug">
-.home-page
+.home-page.full-width.window-height.flex.justify-center.items-center
+  .flex.column.items-center.full-width
+    q-img.rounded-borders(:src="'assets/logo.png'" width="140px" height="140px")
+    .text-h4.q-mt-sm BelkaScope
+    q-input.q-mt-xl.rounded-border.full-width(standout dense v-model="search" :style="'max-width: 360px;'")
+      template(v-slot:append)
+        q-btn(flat dense icon="search")
+        q-btn.q-ml-sm(flat dense icon="center_focus_weak")
+
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+
+interface HomePageData {
+  search: string;
+}
 
 export default defineComponent({
   name: 'HomePage',
@@ -12,8 +24,10 @@ export default defineComponent({
 
   components: {},
   
-  data() {
-    return {}
+  data():HomePageData {
+    return {
+      search: ''
+    }
   },
 
   computed: {},
