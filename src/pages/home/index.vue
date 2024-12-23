71f6bdc0c576065d1481a70049ce10017b38113c
Dmitriy E <dsanichwebdev@gmail.com>
2024-12-23 23:45:44 +0300

    Added SearchProducts component

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,15 +1,16 @@
 <template lang="pug">
 .home-page.full-width.window-height.flex.justify-center.items-center
   .flex.column.items-center.full-width
-    q-img.rounded-borders(:src="'assets/logo.png'" width="140px" height="140px")
+    q-img.rounded-borders(:src="'assets/logo.png'" :width="$q.screen.gt.md ? '140px' : '100px'")
     .text-h4.q-mt-sm BelkaScope
-    AuthSearchInput
-
+    AuthSearchInput(v-if="$q.screen.gt.sm")
+    SearchProducts.q-mt-md(v-if="!$q.screen.gt.sm")
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import AuthSearchInput from 'src/components/auth-search-input/index.vue'
+import SearchProducts from 'src/components/search-products/index.vue'
 
 interface HomePageData {
   search: string
@@ -22,6 +23,7 @@ export default defineComponent({
 
   components: {
     AuthSearchInput,
+    SearchProducts
   },
 
   data(): HomePageData {
