7c2a0d0c5f4366bc07e14116d9a73fe650cdad9d
Dmitriy E <dsanichwebdev@gmail.com>
2024-12-25 23:28:19 +0300

    Added AuthButton in /auth page

--- src/pages/auth/index.vue
+++ src/pages/auth/index.vue
@@ -1,16 +1,25 @@
 <template lang="pug">
-.auth-page
+.auth-page.full-width.window-height.flex.justify-center.items-center
+  .flex.column.items-center.full-width
+    q-img.rounded-borders(:src="'assets/logo.png'" :width="$q.screen.gt.md ? '140px' : '100px'")
+    .text-h4.q-mt-sm BelkaScope
+    .q-mt-md
+      .text-h6.text-weight-regular Войти с помощью
+      AuthButtons
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import AuthButtons from 'src/components/auth-buttons/index.vue'
 
 export default defineComponent({
   name: 'AuthPage',
 
   props: {},
 
-  components: {},
+  components: {
+    AuthButtons,
+  },
 
   data() {
     return {}
