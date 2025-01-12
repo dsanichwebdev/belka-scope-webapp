71aea7dc5da1d8055cdd8a70d9e3d132ec89e06f
Dmitriy E <dsanichwebdev@gmail.com>
2025-01-12 14:46:18 +0300

    Added header component

---
+++ src/components/header/index.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+q-header(elevated)
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	name: 'HeaderComponent',
+	props: {},
+	setup() {},
+})
+</script>
+
+<style scoped lang="stylus"></style>
