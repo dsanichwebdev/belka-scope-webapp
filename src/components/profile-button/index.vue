42037669fcee436d70b1d6b1c0f9df3f6843d466
Dmitriy E <dsanichwebdev@gmail.com>
2025-01-14 22:23:00 +0300

    Added profile-button component

---
+++ src/components/profile-button/index.vue
@@ -0,0 +1,69 @@
+<template lang="pug">
+.profile-button.cursor-pointer
+	q-avatar.bg-primary(
+		round
+		@mouseenter="showMenu = true"
+		@mouseleave="handleAvatarLeave"
+	)
+		img(:src="avatarUrl || defaultAvatar")
+		q-menu
+			q-list
+				q-item(clickable @click="goToAccount")
+					q-item-section Аккаунт
+				q-item(clickable @click="goToPayments")
+					q-item-section Платежи
+				q-item(clickable @click="logOut")
+					q-item-section Выйти
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import { authUtil } from 'src/utils/auth.util'
+import { useRouter } from 'vue-router'
+
+export default defineComponent({
+	name: 'ProfileButton',
+
+	setup() {
+		const route = useRouter()
+
+		const avatarUrl = ref<string | null>(null)
+		const defaultAvatar =
+			'https://gravatar.com/avatar/d41d8cd98f00b204e9800998ecf8427e?s=400&d=robohash&r=x'
+
+		const goToAccount = (): void => {
+			route.push('profile')
+		}
+
+		const goToPayments = (): void => {
+			route.push('payments')
+		}
+
+		const logOut = (): void => {
+			console.log('Выход из аккаунта')
+			authUtil.logOut()
+		}
+
+		return {
+			avatarUrl,
+			defaultAvatar,
+			goToAccount,
+			goToPayments,
+			logOut,
+		}
+	},
+})
+</script>
+
+<style scoped lang="stylus">
+.profile-button
+  position: relative
+  display: inline-block
+  q-avatar
+    cursor: pointer
+  q-popup-proxy
+    position: absolute
+    top: 100%
+    left: 0
+    z-index: 1000
+</style>
