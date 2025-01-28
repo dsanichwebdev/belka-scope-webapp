e0b8e6d3640587128db47f8bf73c319c3fa7990a
Dmitriy E <dsanichwebdev@gmail.com>
2025-01-28 16:12:07 +0300

    Added user data in profile settings

--- src/pages/profile/index.vue
+++ src/pages/profile/index.vue
@@ -1,6 +1,9 @@
 <template lang="pug">
 .profile-page.q-px-md.q-pt-lg.q-pb-md.flex.column
 	.text-h4.text-semibold.q-mb-md Личный кабинет
+	.row.full-width.q-mb-md
+		q-card.q-pa-md.full-width
+			SettingsUserData
 	.q-gutter-md(:class="{'row': Screen.gt.sm}")
 		.col
 			ProfileSubsriptions
@@ -13,12 +16,14 @@ import { defineComponent } from 'vue'
 import { Screen } from 'quasar'
 import ProfileSubsriptions from 'src/components/settings/profile-subscriptions/index.vue'
 import PaymentMethod from 'src/components/settings/payment-method/index.vue'
+import SettingsUserData from 'src/components/settings/user-data/index.vue';
 
 export default defineComponent({
 	name: 'ProfilePage',
 	components: {
 		ProfileSubsriptions,
 		PaymentMethod,
+		SettingsUserData
 	},
 	setup() {
 		return {
