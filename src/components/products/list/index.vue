f3a38a3c8164edb014e7dacc733389aa62e117e9
Dmitriy E <dsanichwebdev@gmail.com>
2025-01-31 16:28:16 +0300

    Added viewAllProducts handle button

--- src/components/products/list/index.vue
+++ src/components/products/list/index.vue
@@ -1,13 +1,15 @@
 <template lang="pug">
-.products-list
-	template(v-for="product in data" :key="`${product.title} ${product.seller}`")
-		ProductCard.q-mb-md(:product="product")
+.products-list.q-pb-lg.flex.justify-center
+	template(v-for="product, index in data" :key="`${product.title} ${product.seller}`")
+		ProductCard.q-mb-md.full-width(v-if="index < 4" :product="product")
+	q-btn(v-if="products.length > 4" no-caps @click="viewAllProductsHandle") Смотреть все
 </template>
 
 <script lang="ts">
 import { defineComponent, computed, type PropType } from 'vue'
 import ProductCard from '../card/index.vue';
 import { type Product } from '../../../types/search-products';
+import { useRouter } from 'vue-router';
 
 export default defineComponent({
 	name: 'ProductsList',
@@ -25,9 +27,15 @@ export default defineComponent({
 
 	setup(props) {
 		const data = computed(() => props.products)
+		const router = useRouter()
+
+		const viewAllProductsHandle = () => {
+			router.push({ name: 'products' })
+		}
 
 		return {
-			data
+			data,
+			viewAllProductsHandle
 		}
 	}
 })
