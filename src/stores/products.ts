d6794c81cd2fc4e954acd41d0b913c8b59fc7380
Dmitriy E <dsanichwebdev@gmail.com>
2025-02-06 15:38:14 +0300

    Init products store

---
+++ src/stores/products.ts
@@ -0,0 +1,21 @@
+import { defineStore, acceptHMRUpdate } from 'pinia';
+import { productsResponse } from 'src/mock/products-response.mock'; // Dumb data
+import { type Product } from '../types/search-products';
+
+export const useProductsStore = defineStore('myStore', {
+  state: () => ({}),
+  getters: {},
+  actions: {
+		searchProducts(photo: string, description: string):Product[] {
+			console.log(photo, description)
+			// TODO: some logic for getting products
+
+			// dumb realization
+			return productsResponse;
+		},
+	}
+});
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useProductsStore, import.meta.hot));
+}
