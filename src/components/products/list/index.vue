6df2f9643991ede4ca6aa20152e7e7420dc2ae48
Dmitriy E <dsanichwebdev@gmail.com>
2025-02-11 18:02:16 +0300

    Added product card UI for list mode

--- src/components/products/list/index.vue
+++ src/components/products/list/index.vue
@@ -26,7 +26,7 @@
           q-spinner-dots(color="primary" size="40px")
       .row.q-col-gutter-md
         .col-12.col-sm-6.col-md-2(v-for="product in displayedData" :key="`${product.title} ${product.seller}`")
-          ProductCard.full-width(:product="product" :grid="internalGrid")
+          ProductCard.full-width(:product="product" :grid="internalGrid" :minimize="minimizeCards")
 
   template(v-else)
     template(v-if="hasViewAllButton")
@@ -34,7 +34,7 @@
         ProductCard.q-mb-md.full-width(v-if="index < 4" :product="product")
     template(v-else)
       template(v-for="product in displayedData" :key="`${product.title} ${product.seller}`")
-        ProductCard.q-mb-md.full-width(:product="product")
+        ProductCard.q-mb-md.full-width(:product="product" :minimize="minimizeCards")
 
   q-btn(
     v-if="showViewAllButton"
@@ -45,108 +45,115 @@
 
 <script lang="ts">
 import { defineComponent, computed, type PropType, ref, watch, onMounted } from 'vue'
-import ProductCard from '../card/index.vue';
-import { type Product } from '../../../types/search-products';
-import { useRouter } from 'vue-router';
+import ProductCard from '../card/index.vue'
+import { type Product } from '../../../types/search-products'
+import { useRouter } from 'vue-router'
 
 export default defineComponent({
-  name: 'ProductsList',
+	name: 'ProductsList',
 
-  components: {
-    ProductCard
-  },
+	components: {
+		ProductCard,
+	},
 
-  props: {
-    products: {
-      type: Array as PropType<Product[]>,
-      required: true
-    },
-    hasViewAllButton: {
-      type: Boolean,
-      default: false
-    },
-    grid: {
-      type: Boolean,
-      default: undefined
-    },
-    chunkSize: {
-      type: Number,
-      default: 6
-    },
-    showToggle: {
-      type: Boolean,
-      default: false
-    }
-  },
+	props: {
+		products: {
+			type: Array as PropType<Product[]>,
+			required: true,
+		},
+		hasViewAllButton: {
+			type: Boolean,
+			default: false,
+		},
+		grid: {
+			type: Boolean,
+			default: undefined,
+		},
+		chunkSize: {
+			type: Number,
+			default: 6,
+		},
+		showToggle: {
+			type: Boolean,
+			default: false,
+		},
+		minimizeCards: {
+			type: Boolean,
+			default: false,
+		},
+	},
 
-  setup(props) {
-    const router = useRouter()
-    const loadedChunks = ref(1)
-    const isLoading = ref(false)
-    const internalGrid = ref<boolean>(true)
+	setup(props) {
+		const router = useRouter()
+		const loadedChunks = ref(1)
+		const isLoading = ref(false)
+		const internalGrid = ref<boolean>(true)
 
-    onMounted(() => {
-      if (props.grid !== undefined) {
-        internalGrid.value = props.grid
-      } else {
-        const savedGrid = localStorage.getItem('productViewMode')
-        internalGrid.value = savedGrid === 'list' ? false : true
-      }
-    })
+		onMounted(() => {
+			if (props.grid !== undefined) {
+				internalGrid.value = props.grid
+			} else {
+				const savedGrid = localStorage.getItem('productViewMode')
+				internalGrid.value = savedGrid === 'list' ? false : true
+			}
+		})
 
-    watch(() => props.grid, (newVal) => {
-      if (newVal !== undefined) {
-        internalGrid.value = newVal
-      }
-    })
+		watch(
+			() => props.grid,
+			(newVal) => {
+				if (newVal !== undefined) {
+					internalGrid.value = newVal
+				}
+			},
+		)
 
-    watch(internalGrid, (newVal) => {
-      if (props.grid === undefined) {
-        localStorage.setItem('productViewMode', newVal ? 'grid' : 'list')
-      }
-    })
+		watch(internalGrid, (newVal) => {
+			if (props.grid === undefined) {
+				localStorage.setItem('productViewMode', newVal ? 'grid' : 'list')
+			}
+		})
 
-    const displayedData = computed(() => {
-      if (internalGrid.value) {
-        return props.products.slice(0, loadedChunks.value * props.chunkSize)
-      }
-      return props.products
-    })
+		const displayedData = computed(() => {
+			if (internalGrid.value) {
+				return props.products.slice(0, loadedChunks.value * props.chunkSize)
+			}
+			return props.products
+		})
 
-    const hasMoreData = computed(() =>
-      internalGrid.value && props.products.length > loadedChunks.value * props.chunkSize
-    )
+		const hasMoreData = computed(
+			() => internalGrid.value && props.products.length > loadedChunks.value * props.chunkSize,
+		)
 
-    const showViewAllButton = computed(() =>
-      !internalGrid.value && props.products.length > 4 && props.hasViewAllButton
-    )
+		const showViewAllButton = computed(
+			() => !internalGrid.value && props.products.length > 4 && props.hasViewAllButton,
+		)
 
-    const viewAllProductsHandle = () => {
-      router.push({ name: 'products' })
-    }
+		const viewAllProductsHandle = () => {
+			router.push({ name: 'products' })
+		}
 
-    const onLoad = (index: number, done: (stop?: boolean) => void) => {
-      if (!internalGrid.value || isLoading.value || !hasMoreData.value) {
-        done(true)
-        return
-      }
+		const onLoad = (index: number, done: (stop?: boolean) => void) => {
+			if (!internalGrid.value || isLoading.value || !hasMoreData.value) {
+				done(true)
+				return
+			}
 
-      isLoading.value = true
-      setTimeout(() => {
-        loadedChunks.value++
-        isLoading.value = false
-        done(!hasMoreData.value)
-      }, 500)
-    }
+			isLoading.value = true
+			setTimeout(() => {
+				loadedChunks.value++
+				isLoading.value = false
+				done(!hasMoreData.value)
+			}, 500)
+		}
 
-    return {
-      displayedData,
-      hasMoreData,
-      showViewAllButton,
-      viewAllProductsHandle,
-      onLoad,
-      internalGrid
-    }
-  }
+		return {
+			displayedData,
+			hasMoreData,
+			showViewAllButton,
+			viewAllProductsHandle,
+			onLoad,
+			internalGrid,
+		}
+	},
 })
 </script>
